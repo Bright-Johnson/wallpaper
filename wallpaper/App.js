94cb185ccb50f5157aa1d9f719e0eda7dcffb841
bright johnson <js.bright93@gmail.com>
2019-08-29 19:05:25 +0530

    This is for making album

--- wallpaper/App.js
+++ wallpaper/App.js
@@ -1,69 +1,20 @@
+// Index.ios.js - place code in here for IOS!!!!
+
+// Import a library to help create a component
 import React from 'react';
-import { StyleSheet, View, ActivityIndicator, FlatList, Dimensions, Image } from 'react-native';
-import axios from 'axios';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
-const { height, width } = Dimensions.get('window');
-export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      isLoading: true,
-      images: []
-    };
-    this.loadWallpapers = this.loadWallpapers.bind(this);
-    this.renderItem = this.renderItem.bind(this);
-  }
-    componentDidMount() {
-      this.loadWallpapers();
-    }
-    loadWallpapers() {
-      axios.get('http://api.unsplash.com/photos/random?count=30&clienet_id=b650ab0da0d684db4b2f8c9e8eeeb9643b0c21cbe7f39ea63fc60fcec2e62b31')
-      .then((response) => {
-        console.log(response.data);
-        this.setState({ images: response.data, isLoading: false });
-      }).bind(this)
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        console.log('request completed');
-      });
-    }
-    renderItem(image) {
-      return (
-        <View style={{ height, width }}>
-          <Image
-              style={{ flex: 1, height: null, width: null }}
-              source={{ uri: image.urls.regular }}
-              resizeMode="cover"
-          />
-       </View>
-     );
-  }
-  render() {
-  return this.state.isLoading ? (
-    <View style={styles.container}>
-      <ActivityIndicator size="large" color="grey" />
-    </View>
-  ) : (
-    <View style={{ flex: 1, backgroundColor: 'black' }}>
-            <FlatList
-            horizontal
-            pagingEnabled
-            data={this.state.images}
-            renderItem={(({ item }) => this.renderItem(item))}
-            keyExtractor={item => item.id}
-            />
-            </View>
-  );
-}
-}
+// Create a component
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    justifyContent: 'center',
 
-  },
-});
+export default App;
+// Render it to the device
+AppRegistry.registerComponent('wallpaper', () => App);
